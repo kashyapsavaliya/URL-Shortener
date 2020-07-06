6c724dcaa2f5bb8889421acc94898e7a3805db64
kashyapsavaliya <kkashyap.savaliya@gmail.com>
2020-07-05 20:25:04 -0400

    Add style and favicon

--- background.js
+++ background.js
@@ -1,23 +1,20 @@
 function getShortURL() {
 
-    chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.getSelected(null, function (tab) {
 
         // grabbing the tab url here
         var tablink = tab.url;
         console.log("LONG URL : " + tablink);
 
         // API 
-        var accessToken = 'add token here';
+        var accessToken = 'add api key here';
         var url = 'https://api-ssl.bitly.com/v3/shorten?access_token=' + accessToken + '&longUrl=' + encodeURIComponent(tablink);
 
-        jQuery.getJSON(url, function(response) {
-
+        jQuery.getJSON(url, function (response) {
             console.log("SHORT URL : " + response.data.url);
             var element = document.getElementById("url");
             element.innerHTML = response.data.url;
-
         });
-
     });
 }
 
@@ -36,4 +33,3 @@ function copy() {
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("copy").onclick = copy;
 });
-
